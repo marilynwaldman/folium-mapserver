f0abb2e063e08d685332d9da5c847b8511785dab
MarilynWaldman <mdwaldman22@gmail.com>
2021-12-01 09:19:06 -0700

    fixed caching problem

--- tracker/app.py
+++ tracker/app.py
@@ -38,7 +38,22 @@ nav.register_element('top', topbar)
 app = Flask(__name__)
 Bootstrap(app)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+map_dict = {  'CenCal.html' : ['California Water Districts and Pits','Top 3 producers'],
+              'california_wellcanal.html' : ['California Wells', ' map by Todd']
+}
 app.vars = {}
+
+def nocache(view):
+  @wraps(view)
+  def no_cache(*args, **kwargs):
+    response = make_response(view(*args, **kwargs))
+    response.headers['Last-Modified'] = datetime.now()
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
+        
+  return update_wrapper(no_cache, view)
 
 
 @app.route('/')
@@ -52,7 +67,9 @@ def index():
     map_name = f"CenCal.html"
     #have to set map path - used by template
     map_path = os.path.join(app.root_path, 'static/' + map_name)
+    print(map_path)
     app.vars['map_path'] = map_path
+    print(app.vars['map_path'])
     app.vars['Title_line1'] = "California Water Districts and Pits"
     app.vars['Title_line2'] = "Top 3 producers"
 
@@ -66,16 +83,19 @@ def index():
 
 @app.route('/map', methods=['GET'])
 # http://localhost:5000/map?map=CenCal.html
-# http://localhost:5000/map?map=CAWaterDistrictMap.html
+# http://localhost:5000/map?map=california_wellcanal.html
+@nocache
 def get_map():
   args = request.args
   print (args) # For debugging
   map_name = args['map']
   print(type(map_name))
   map_path = os.path.join(app.root_path, 'static/' + map_name)
+  print(map_path)
   app.vars['map_path'] = map_path
-  app.vars['Title_line1'] = "California Water Districts and Pits"
-  app.vars['Title_line2'] = "Top 3 producers"
+  app.vars['Title_line1'] = map_dict[map_name][0]
+  app.vars['Title_line2'] = map_dict[map_name][1]
+  
   if Path(map_path).exists():
         return render_template('display.html', vars=app.vars)
   else:     
@@ -84,9 +104,11 @@ def get_map():
   pass
 
 @app.route('/maps/map.html')
-
+@nocache
 def show_map():
   map_path = app.vars.get("map_path")
+  print("show map")
+  print(map_path)
   map_file = Path(map_path)
   if map_file.exists():
     return send_file(map_path)
